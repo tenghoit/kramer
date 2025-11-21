9c5e1fac68354eed156bbcb48bd9d3986c453736
Tenghoit Kouch <tenghoitkouch+github@gmail.com>
2025-11-21 08:45:14 -0500

    dblib

--- src/db/dblib.py
+++ src/db/dblib.py
@@ -6,23 +6,26 @@ from chromadb.utils.embedding_functions import OllamaEmbeddingFunction # type: i
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+
 def get_collection(name: str) -> chromadb.Collection | None: # type: ignore
-    client = chromadb.PersistentClient(path="/") # type: ignore
     collections = client.list_connections()
     if name in collections:
         logger.debug(f"Collection found: {name}")
         return client.get_collection(name=name)
     else:
+        logger.debug(f"Collection not found: {name}")
         return None
 
 
 def create_collection(name: str, model: str = "qwen3-embedding:8b") -> chromadb.Collection | None: # type: ignore
-    client = chromadb.PersistentClient(path="/") # type: ignore
+    """
+    Create a new Chromadb collection.
+    If collection already exists, return existing collection
+    """
     collection = get_collection(name)
     if collection is not None: return collection
 
     logger.debug(f"Creating collection: {name}")
-
     collection = client.create_collection(
         name=name,
         embedding_function=OllamaEmbeddingFunction(
@@ -31,5 +34,31 @@ def create_collection(name: str, model: str = "qwen3-embedding:8b") -> chromadb.
         ),
         configuration={"hnsw": {"space": "cosine"}}
     )
+    logger.debug(f"Collection created: {name}")
     return collection
+
+
+def delete_collection(name: str):
+    collection = get_collection(name)
+    if collection is None:
+        logger.debug(f"Deletion Failed. Collection does not exist: {name}")
+        return
+    client.delete_collection(name=name)
+    logger.debug(f"Collection deleted: {name}")
+    return
+
+
+def get_notes(class_code: str, topic: str):
+    notes = get_collection("notes")
+    metadata = {
+        "$and": [
+            {"class_code": class_code},
+            {"topic": topic}
+        ]
+    }
+    results = notes.query(
+        query_embeddings=[],
+        where=metadata
+    )
+    
 
